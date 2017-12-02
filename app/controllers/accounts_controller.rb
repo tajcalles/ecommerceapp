87e34684a599a04ceab276b2e813243532a436fe
Taj Calles <tajcalles@gmail.com>
2017-12-02 09:01:52 -0700

    fixed prev server error

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,6 @@
 class AccountsController < ApplicationController
-  # CREATED BY DEVISE
+  before_action :set_account, except: [:index, :new, :create]
+
   def index
   end
 
@@ -9,6 +10,27 @@ class AccountsController < ApplicationController
   def new
   end
 
-  def edi
+  def create
+
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :email)
+  end
+
+  def set_account
+    @account = current_user.accounts.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
   root 'accounts#index'
-  resources :items
-  # resources :carts
   resources :accounts
-  devise_for :users
-
-  resources :users do
-    resources :carts
-  end
-
+  
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
